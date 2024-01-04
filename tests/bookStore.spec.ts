e0389bc0950eee294036856fd6249a831f9e6c59
Maheshwaran00 <maheshwaran0462@gmail.com>
2024-01-04 15:35:11 +0530

    changes committed

--- tests/bookStore.spec.ts
+++ tests/bookStore.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect, takeArchive } from "@chromaui/test-archiver";
 
+test.use({ disableAutoCapture: true,resourceArchiveTimeout:1000*60 });
+
 test("Book Store", async ({ page }, testInfo) => {    
   await page.goto("https://automationbookstore.dev/");
 
-  await expect(page).toHaveTitle('STORE');
+  await expect(page).toHaveTitle('Automation Bookstore');
   
   await takeArchive(page, 'Home Page', testInfo);
 
